0f1f01921600c26845848e4181ff66c759b55418
sgr-ksmt <suguru-kishimoto@cookpad.com>
2018-10-07 15:31:00 +0900

    added sample code.

--- main.playground/Contents.swift
+++ main.playground/Contents.swift
@@ -5,3 +5,12 @@ import RxSwift
 import RxCocoa
 import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
+
+let relay = PublishRelay<Int>()
+
+relay.accept(1)
+
+relay.subscribe(onNext: { print($0) })
+
+relay.accept(2)
+relay.accept(3)
